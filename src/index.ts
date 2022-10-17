76c063269839c206b322c8a71f1c9dfba2249cf1
Qiquan Lu <kejeon@gmail.com>
2022-10-17 15:36:37 -0400

    updat

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { App } from "vue";
 import VBarcode from "./VBarcode";
 
-export default {
-  install(app: App) {
-    app.component("VBarcode", VBarcode);
-  },
-  VBarcode,
-};
+// export default {
+//   install(app: App) {
+//     app.component("VBarcode", VBarcode);
+//   },
+//   VBarcode,
+// };
 export { default as VBarcode, vBarcodeProps } from "./VBarcode";
 
 export type { VBarcodeProps, BarcodeFormat } from "./VBarcode";
